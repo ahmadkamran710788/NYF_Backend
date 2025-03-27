f75752ba2b33dbfca06a9f6a7409c878182e9eeb
Ahmad <mac@Macs-MacBook-Pro.local>
2025-03-27 05:43:59 +0500

    done with the activity detail

--- src/models/ActivityDetails.ts
+++ src/models/ActivityDetails.ts
@@ -1,43 +1,3 @@
-
-// import mongoose, { Schema, Document } from "mongoose";
-
-// export interface IActivityDetail extends Document {
-//   activityId: mongoose.Types.ObjectId;
-  
-//   itinerary: string[];
-//   policy: {
-//     cancellation: string;
-//     ageRestriction: string;
-//   };
-//   additionalInfo: string[];
-//   faq: Array<{ question: string; answer: string }>;
-// }
-
-// const ActivityDetailSchema = new Schema({
-//   activityId: { 
-//     type: Schema.Types.ObjectId, 
-//     ref: "Activity", 
-//     required: true,
-//     unique: true 
-//   },
-  
-//   itinerary: [{ type: String }],
-//   policy: {
-//     cancellation: { type: String },
-//     ageRestriction: { type: String }
-//   },
-//   additionalInfo: [{ type: String }],
-//   faq: [{
-//     question: { type: String },
-//     answer: { type: String }
-//   }]
-// });
-
-// export const ActivityDetail = mongoose.model<IActivityDetail>("ActivityDetail", ActivityDetailSchema);
-
-
-
-
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IActivityDetail extends Document {
@@ -91,7 +51,8 @@ const ActivityDetailSchema = new Schema({
   },
   howToGetThere: [{ type: String }],
   howToRedeem: [{ type: String }],
-  cancellationPolicy: [{ type: String }]
+  cancellationPolicy: [{ type: String }],
+
 });
 
 export const ActivityDetail = mongoose.model<IActivityDetail>("ActivityDetail", ActivityDetailSchema);
